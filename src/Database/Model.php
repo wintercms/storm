865909ca2de5f1896a2d70d3571fde9a763732fc
Marc Jauvin <marc.jauvin@gmail.com>
2021-04-19 10:40:17 -0400

    add empty line between both notes

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -300,6 +300,7 @@ class Model extends EloquentModel
          * @event model.beforeSave
          * Called before the model is saved
          * > **Note:** This is called both when creating and updating
+         *
          * > **Note:** also triggered in Winter\Storm\Halcyon\Model
          *
          * Example usage:
