69990ab3aead34cb930fee35757fc9acc7a1fd0c
flynsarmy <flynsarmy@gmail.com>
2021-04-29 10:33:58 +1000

    Add tests

--- tests/Database/Traits/ValidationTest.php
+++ tests/Database/Traits/ValidationTest.php
@@ -55,6 +55,21 @@ class ValidationTest extends TestCase
         $this->assertEquals([
             'email' => ['unique:mysql.users,email_address,20,id,account_id,1']
         ], $this->processValidationRules($rules));
+
+        /*
+         * Adding multiple additional where clauses
+         */
+        $rules = ['email' => 'unique:users,email_address,NULL,id,account_id,1,account_name,"Foo",user_id,3'];
+
+        $this->exists = true;
+        $this->assertEquals([
+            'email' => ['unique:mysql.users,email_address,20,id,account_id,1,account_name,"Foo",user_id,3']
+        ], $this->processValidationRules($rules));
+
+        $this->exists = false;
+        $this->assertEquals([
+            'email' => ['unique:mysql.users,email_address,20,id,account_id,1,account_name,"Foo",user_id,3']
+        ], $this->processValidationRules($rules));
     }
 
     protected function getConnectionName()
