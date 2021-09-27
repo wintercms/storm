90fcb80219866e81fe4acb4cf1c9f080db014181
Luke Towers <github@luketowers.ca>
2021-09-26 21:38:14 -0600

    Update src/Mail/Mailer.php

--- src/Mail/Mailer.php
+++ src/Mail/Mailer.php
@@ -121,7 +121,7 @@ class Mailer extends MailerBase
          *
          * Example usage (logs the message):
          *
-         *     Event::listen('mailer.send', function ((\Winter\Storm\Mail\Mailer) $mailerInstance, (string) $view,  (\Illuminate\Mail\Message) $message, (array) $data) {
+         *     Event::listen('mailer.send', function ((\Winter\Storm\Mail\Mailer) $mailerInstance, (string) $view, (\Illuminate\Mail\Message) $message, (array) $data) {
          *         \Log::info("Message was rendered with $view and sent");
          *     });
          *
