e12778131fcf740b66c1e159a2206c9835b2866c
Luke Towers <github@luketowers.ca>
2021-09-26 21:36:54 -0600

    Apply suggestions from code review

--- src/Mail/Mailer.php
+++ src/Mail/Mailer.php
@@ -91,8 +91,7 @@ class Mailer extends MailerBase
           *
           * Example usage (stops the sending process):
           *
-          *     Event::listen('mailer.prepareSend', function ((\Winter\Storm\Mail\Mailer) $mailerInstance, (string) $view,
-          *     (\Illuminate\Mail\Message) $message, (array) $data) {
+          *     Event::listen('mailer.prepareSend', function ((\Winter\Storm\Mail\Mailer) $mailerInstance, (string) $view (\Illuminate\Mail\Message) $message, (array) $data) {
           *         return false;
           *     });
           *
@@ -122,8 +121,7 @@ class Mailer extends MailerBase
          *
          * Example usage (logs the message):
          *
-         *     Event::listen('mailer.send', function ((\Winter\Storm\Mail\Mailer) $mailerInstance, (string) $view,
-               (\Illuminate\Mail\Message) $message, (array) $data) {
+         *     Event::listen('mailer.send', function ((\Winter\Storm\Mail\Mailer) $mailerInstance, (string) $view,  (\Illuminate\Mail\Message) $message, (array) $data) {
          *         \Log::info("Message was rendered with $view and sent");
          *     });
          *
