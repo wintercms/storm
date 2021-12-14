14842fa2b0cbfd8e23c7cbe5fc12715f05c59142
Luke Towers <github@luketowers.ca>
2021-12-14 14:51:26 -0600

    Fix Sortable trait & test
    Credit to @inxilpro, see https://github.com/laravel/framework/pull/37956#issuecomment-993955917

--- tests/Database/SortableTest.php
+++ tests/Database/SortableTest.php
@@ -12,11 +12,6 @@ class SortableTest extends DbTestCase
 
     public function testOrderByCanBeOverridden()
     {
-        // @TODO: Fix, see https://github.com/laravel/framework/pull/37956#issuecomment-993822397
-        // May have to override orderBy explicitly in the Winter Builder and then check for defined
-        // macros there before passing to parent to handle
-        $this->markTestSkipped('@TODO: Failing');
-
         $model = new TestSortableModel();
         $query1 = $model->newQuery()->orderBy('name')->orderBy('email', 'desc')->toSql();
         $query2 = $model->newQuery()->orderBy('sort_order')->orderBy('name')->toSql();
