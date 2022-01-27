87d2d9c0a45d50dc67a404a298e55b599a8f9249
Jack Wilkinson <me@jackwilky.com>
2022-01-27 13:32:32 +0000

    Fixed incorrect comment

--- src/Config/ConfigFunction.php
+++ src/Config/ConfigFunction.php
@@ -1,7 +1,7 @@
 <?php namespace Winter\Storm\Config;
 
 /**
- * Class EnvFile
+ * Class ConfigFunction
  * @package Winter\Storm\Config
  *
  * This class is for use with ConfigFile as a method to inject a function call into a config file
