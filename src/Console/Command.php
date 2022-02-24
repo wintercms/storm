ce3ead9730d5138d78f8e1a2165c78f213671cea
Luke Towers <luke@luketowers.ca>
2022-02-23 19:50:13 -0600

    Add Command base class
    Adds the Winter\Storm\Console\Command base class that provides some utilities to make developing CLI commands easier.
    
    Commands can extend this class and then provide completion / suggestions for arguments / options by adding suggest{$name}[Values|Options] methods for each of the arguments / options that it wants to provide suggestions for.

---
+++ src/Console/Command.php
@@ -0,0 +1,62 @@
+<?php namespace Winter\Storm\Console;
+
+use Winter\Storm\Support\Str;
+use Illuminate\Console\Command as BaseCommand;
+use Symfony\Component\Console\Completion\CompletionInput;
+use Symfony\Component\Console\Completion\CompletionSuggestions;
+
+/**
+ * Command base class
+ * Contains utilities to make developing CLI commands nicer
+ *
+ * @author Luke Towers
+ */
+abstract class Command extends BaseCommand
+{
+    /**
+     * Provide autocompletion for this command's input
+     */
+    public function complete(CompletionInput $input, CompletionSuggestions $suggestions): void
+    {
+        $inputs = [
+            'arguments' => $input->getArguments(),
+            'options'   => $input->getOptions(),
+        ];
+
+        foreach ($inputs as $type => $data) {
+            switch ($type) {
+                case 'arguments':
+                    $dataType = 'Argument';
+                    $suggestionType = 'Values';
+                    break;
+                case 'options':
+                    $dataType = 'Option';
+                    $suggestionType = 'Options';
+                    break;
+            }
+            if (!empty($data)) {
+                foreach ($data as $name => $value) {
+                    // Skip the command argument since that's handled by Artisan directly
+                    if (
+                        $type === 'arguments'
+                        && in_array($name, ['command'])
+                    ) {
+                        continue;
+                    }
+
+                    $inputRoutingMethod = "mustSuggest{$dataType}ValuesFor";
+                    $suggestionValuesMethod = Str::camel('suggest ' . $name) . $suggestionType;
+                    $suggestionsMethod = 'suggest' . $suggestionType;
+
+                    if (
+                        method_exists($this, $suggestionValuesMethod)
+                        && $input->{$inputRoutingMethod}($name)
+                    ) {
+                        $values = $this->$suggestionValuesMethod($value);
+                        $suggestions->{$suggestionsMethod}($values);
+                    }
+                }
+            }
+        }
+    }
+}
