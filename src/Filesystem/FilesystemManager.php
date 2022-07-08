a84725d84a7d35ab270fd9e866d25ee4c450ad0c
Luke Towers <github@luketowers.ca>
2022-07-08 15:51:29 -0600

    Update src/Filesystem/FilesystemManager.php

--- src/Filesystem/FilesystemManager.php
+++ src/Filesystem/FilesystemManager.php
@@ -31,6 +31,9 @@ class FilesystemManager extends BaseFilesystemManager
         if (is_null($config)) {
             $config = $this->getConfig($name);
         }
+
+        // Default local drivers to public visibility for backwards compatibility
+        // see https://github.com/wintercms/winter/issues/503
         if ($config['driver'] === 'local' && empty($config['visibility'])) {
             $config['visibility'] = 'public';
         }
