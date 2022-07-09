2cd478049899b13a8b8254b9d750346e760819df
Luke Towers <github@luketowers.ca>
2022-07-08 23:53:21 -0600

    Update src/Support/helpers.php

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -20,7 +20,7 @@ if (!function_exists('e')) {
             return $value->toHtml();
         }
 
-        return htmlspecialchars($value ?? '', ENT_QUOTES, 'UTF-8', $doubleEncode);
+        return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8', $doubleEncode);
     }
 }
 
