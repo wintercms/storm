677205ac9452e4be57305222ce1ff9b6f21e6946
Luke Towers <github@luketowers.ca>
2022-07-12 17:54:07 -0600

    Update src/Auth/Manager.php

--- src/Auth/Manager.php
+++ src/Auth/Manager.php
@@ -1,8 +1,8 @@
 <?php namespace Winter\Storm\Auth;
 
-use \Illuminate\Support\Facades\Cookie;
-use \Illuminate\Support\Facades\Session;
-use \Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Contracts\Auth\Authenticatable;
 
 /**
