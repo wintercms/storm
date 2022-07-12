wintercms/storm

commit 1a46e9e9ecea3e71c15cbe8174708724466bcc15

WIP Html module analysis

src/Html/BlockBuilder.php
@@ -9,42 +9,44 @@ use Exception;
  */
 class BlockBuilder
 {
-    protected $blockStack = [];
-    protected $blocks = [];
+    /**
+     * The block stack.
+     */
+    protected array $blockStack = [];
 
     /**
-     * Helper for startBlock
-     *
-     * @param string $name Specifies the block name.
-     * @return void
+     * Registered block contents, keyed by block name.
      */
-    public function put($name)
+    protected array $blocks = [];
+
+    /**
+     * Helper method for the "startBlock" templating function.
+     */
+    public function put(string $name): void
     {
         $this->startBlock($name);
     }
 
     /**
-     * Begins the layout block.
+     * Begins the layout block for a given block name.
      *
      * This method enables output buffering, so all output will be captured as a part of this block.
-     *
-     * @param string $name Specifies the block name.
-     * @return void
      */
-    public function startBlock($name)
+    public function startBlock(string $name): void
     {
         array_push($this->blockStack, $name);
         ob_start();
     }
 
     /**
-     * Helper for endBlock and also clears the output buffer.
+     * Helper method for the "endBlock" templating function.
      *
-     * @param boolean $append Indicates that the new content should be appended to the existing block content.
-     * @return void
+     * If `$append` is `true`, the new content should be appended to an existing block, as opposed to overwriting any
+     * previous content.
+     *
      * @throws \Exception if there are no items in the block stack
      */
-    public function endPut($append = false)
+    public function endPut(bool $append = false): void
     {
         $this->endBlock($append);
     }
@@ -54,11 +56,9 @@ class BlockBuilder
      *
      * This captures all buffered output as the block's content, and ends output buffering.
      *
-     * @param boolean $append Indicates that the new content should be appended to the existing block content.
-     * @return void
      * @throws \Exception if there are no items in the block stack
      */
-    public function endBlock($append = false)
+    public function endBlock(bool $append = false): void
     {
         if (!count($this->blockStack)) {
             throw new Exception('Invalid block nesting');
@@ -75,30 +75,21 @@ class BlockBuilder
     }
 
     /**
-     * Sets a content of the layout block.
+     * Sets a content of the layout block, overwriting any previous content for that block.
      *
      * Output buffering is not used for this method.
-     *
-     * @param string $name Specifies the block name.
-     * @param string $content Specifies the block content.
-     * @return void
-     * @throws \Exception if there are no items in the block stack
      */
-    public function set($name, $content)
+    public function set(string $name, string $content): void
     {
         $this->blocks[$name] = $content;
     }
 
     /**
-     * Appends a content of the layout block.
+     * Appends content to a layout block.
      *
      * Output buffering is not used for this method.
-     *
-     * @param string $name Specifies the block name.
-     * @param string $content Specifies the block content.
-     * @return void
      */
-    public function append($name, $content)
+    public function append(string $name, string $content): void
     {
         if (!isset($this->blocks[$name])) {
             $this->blocks[$name] = '';
@@ -108,13 +99,11 @@ class BlockBuilder
     }
 
     /**
-     * Returns the layout block contents and deletes the block from memory.
+     * Returns the layout block contents of a given block name and deletes the block from memory.
      *
-     * @param string $name Specifies the block name.
-     * @param string $default Specifies a default block value to use if the block requested is not exists.
-     * @return string
+     * If the block does not exist, then the `$default` content will be returned instead.
      */
-    public function placeholder($name, $default = null)
+    public function placeholder(string $name, string $default = ''): string
     {
         $result = $this->get($name, $default);
         unset($this->blocks[$name]);
@@ -127,16 +116,14 @@ class BlockBuilder
     }
 
     /**
-     * Returns the layout block contents but not deletes the block from memory.
+     * Returns the layout block contents of a given name, but does not delete it from memory.
      *
-     * @param string $name Specifies the block name.
-     * @param string $default Specifies a default block value to use if the block requested is not exists.
-     * @return string
+     * If the block does not exist, then the `$default` content will be returned instead.
      */
-    public function get($name, $default = null)
+    public function get(string $name, string $default = ''): string
     {
         if (!isset($this->blocks[$name])) {
-            return  $default;
+            return $default;
         }
 
         return $this->blocks[$name];
@@ -144,10 +131,8 @@ class BlockBuilder
 
     /**
      * Clears all the registered blocks.
-     *
-     * @return void
      */
-    public function reset()
+    public function reset(): void
     {
         $this->blockStack = [];
         $this->blocks = [];
@@ -155,10 +140,8 @@ class BlockBuilder
 
     /**
      * Gets the block stack at this point.
-     *
-     * @return array
      */
-    public function getBlockStack()
+    public function getBlockStack(): array
     {
         return $this->blockStack;
     }

src/Html/FormBuilder.php
@@ -14,88 +14,93 @@ class FormBuilder
 
     /**
      * The HTML builder instance.
-     *
-     * @var \Winter\Storm\Html\HtmlBuilder
      */
-    protected $html;
+    protected \Winter\Storm\Html\HtmlBuilder $html;
 
     /**
      * The URL generator instance.
-     *
-     * @var \Illuminate\Routing\UrlGenerator  $url
      */
-    protected $url;
+    protected \Illuminate\Routing\UrlGenerator $url;
 
     /**
      * The CSRF token used by the form builder.
-     *
-     * @var string
      */
-    protected $csrfToken;
+    protected string $csrfToken;
 
     /**
      * The session store implementation.
-     *
-     * @var \Illuminate\Session\Store
      */
-    protected $session;
+    protected \Illuminate\Session\Store $session;
 
     /**
      * The current model instance for the form.
-     *
-     * @var object|array|null
      */
-    protected $model;
+    protected object|array|null $model = null;
 
     /**
      * An array of label names we've created.
-     *
-     * @var array
      */
-    protected $labels = [];
+    protected array $labels = [];
 
     /**
      * The reserved form open attributes.
-     * @var array
      */
-    protected $reserved = ['method', 'url', 'route', 'action', 'files', 'request', 'model', 'sessionKey'];
+    protected array $reserved = [
+        'method',
+        'url',
+        'route',
+        'action',
+        'files',
+        'request',
+        'model',
+        'sessionKey'
+    ];
 
     /**
-     * The reserved form open attributes.
-     * @var array
+     * The reserved form AJAX attributes.
      */
-    protected $reservedAjax = ['request', 'success', 'error', 'complete', 'confirm', 'redirect', 'update', 'data', 'validate', 'flash'];
+    protected array $reservedAjax = [
+        'request',
+        'success',
+        'error',
+        'complete',
+        'confirm',
+        'redirect',
+        'update',
+        'data',
+        'validate',
+        'flash'
+    ];
 
     /**
      * The form methods that should be spoofed, in uppercase.
-     *
-     * @var array
      */
-    protected $spoofedMethods = ['DELETE', 'PATCH', 'PUT'];
+    protected array $spoofedMethods = [
+        'DELETE',
+        'PATCH',
+        'PUT'
+    ];
 
     /**
      * The types of inputs to not fill values on by default.
-     *
-     * @var array
      */
-    protected $skipValueTypes = ['file', 'password', 'checkbox', 'radio'];
+    protected array $skipValueTypes = [
+        'file',
+        'password',
+        'checkbox',
+        'radio'
+    ];
 
     /**
      * The session key used by the form builder.
      * @var string
      */
-    protected $sessionKey;
+    protected string $sessionKey;
 
     /**
      * Create a new form builder instance.
-     *
-     * @param \Winter\Storm\Html\HtmlBuilder  $html
-     * @param \Illuminate\Routing\UrlGenerator  $url
-     * @param string  $csrfToken
-     * @param string  $sessionKey
-     * @return void
      */
-    public function __construct(HtmlBuilder $html, UrlGeneratorBase $url, $csrfToken, $sessionKey)
+    public function __construct(HtmlBuilder $html, UrlGeneratorBase $url, string $csrfToken, string $sessionKey)
     {
         $this->url = $url;
         $this->html = $html;
@@ -395,7 +400,7 @@ class FormBuilder
     /**
      * Create a select box field with empty option support.
      */
-    public function select(string $name, array $list = [], ?string $selected = null, array $options = []): string
+    public function select(string $name, array $list = [], string|array|null $selected = null, array $options = []): string
     {
         if (array_key_exists('emptyOption', $options)) {
             $list = ['' => $options['emptyOption']] + $list;
@@ -434,7 +439,7 @@ class FormBuilder
     /**
      * Create a select range field.
      */
-    public function selectRange(string $name, string|int|float $begin, string|int|float $end, ?string $selected = null, array $options = []): string
+    public function selectRange(string $name, string|int|float $begin, string|int|float $end, string|array|null $selected = null, array $options = []): string
     {
         $range = array_combine($range = range($begin, $end), $range);
 
@@ -444,7 +449,7 @@ class FormBuilder
     /**
      * Create a select year field.
      */
-    public function selectYear(string $name, int $begin = 1900, ?int $end = null, ?string $selected = null, array $options = []): string
+    public function selectYear(string $name, int $begin = 1900, ?int $end = null, string|array|null $selected = null, array $options = []): string
     {
         if (is_null($end)) {
             $end = (int) date('Y');
@@ -455,7 +460,7 @@ class FormBuilder
     /**
      * Create a select month field.
      */
-    public function selectMonth(string $name, ?string $selected = null, array $options = [], $format = '%B'): string
+    public function selectMonth(string $name, string|array|null $selected = null, array $options = [], $format = '%B'): string
     {
         $months = [];
 
@@ -468,13 +473,8 @@ class FormBuilder
 
     /**
      * Get the select option for the given value.
-     *
-     * @param  string  $display
-     * @param  string  $value
-     * @param  string  $selected
-     * @return string
      */
-    public function getSelectOption(string|array $display, string $value, ?string $selected = null)
+    public function getSelectOption(string|array $display, string $value, string|array|null $selected = null): string
     {
         if (is_array($display)) {
             return $this->optionGroup($display, $value, $selected);
@@ -485,13 +485,8 @@ class FormBuilder
 
     /**
      * Create an option group form element.
-     *
-     * @param  array   $list
-     * @param  string  $label
-     * @param  string  $selected
-     * @return string
      */
-    protected function optionGroup($list, $label, $selected)
+    protected function optionGroup(array $list, string $label, string|array|null $selected = null): string
     {
         $html = [];
 
@@ -499,40 +494,38 @@ class FormBuilder
             $html[] = $this->option($display, $value, $selected);
         }
 
-        return '<optgroup label="'.e($label).'">'.implode('', $html).'</optgroup>';
+        return '<optgroup label="' . e($label) . '">' . implode('', $html) . '</optgroup>';
     }
 
     /**
      * Create a select element option.
-     *
-     * @param  string  $display
-     * @param  string  $value
-     * @param  string  $selected
-     * @return string
      */
-    protected function option($display, $value, $selected)
+    protected function option(string $display, string $value, string|array|null $selected = null): string
     {
-        $selected = $this->getSelectedValue($value, $selected);
+        $selectedAttr = $this->getSelectedValue($value, $selected);
 
-        $options = ['value' => e($value), 'selected' => $selected];
+        $options = [
+            'value' => e($value),
+            'selected' => $selectedAttr
+        ];
 
-        return '<option'.$this->html->attributes($options).'>'.e($display).'</option>';
+        return '<option' . $this->html->attributes($options) . '>' . e($display) . '</option>';
     }
 
     /**
      * Determine if the value is selected.
-     *
-     * @param  string  $value
-     * @param  string  $selected
-     * @return string
      */
-    protected function getSelectedValue($value, $selected)
+    protected function getSelectedValue(string $value, string|array|null $selected): string|null
     {
+        if (is_null($selected)) {
+            return null;
+        }
+
         if (is_array($selected)) {
             return in_array($value, $selected) ? 'selected' : null;
         }
 
-        return ((string) $value == (string) $selected) ? 'selected' : null;
+        return ((string) $value === (string) $selected) ? 'selected' : null;
     }
 
     //
@@ -541,28 +534,16 @@ class FormBuilder
 
     /**
      * Create a checkbox input field.
-     *
-     * @param  string  $name
-     * @param  mixed   $value
-     * @param  bool    $checked
-     * @param  array   $options
-     * @return string
      */
-    public function checkbox($name, $value = 1, $checked = null, $options = [])
+    public function checkbox(string $name, string $value = '1', bool $checked = false, array $options = []): string
     {
         return $this->checkable('checkbox', $name, $value, $checked, $options);
     }
 
     /**
      * Create a radio button input field.
-     *
-     * @param  string  $name
-     * @param  mixed   $value
-     * @param  bool    $checked
-     * @param  array   $options
-     * @return string
      */
-    public function radio($name, $value = null, $checked = null, $options = [])
+    public function radio(string $name, ?string $value = null, bool $checked = false, array $options = []): string
     {
         if (is_null($value)) {
             $value = $name;
@@ -573,15 +554,8 @@ class FormBuilder
 
     /**
      * Create a checkable input field.
-     *
-     * @param  string  $type
-     * @param  string  $name
-     * @param  mixed   $value
-     * @param  bool    $checked
-     * @param  array   $options
-     * @return string
      */
-    protected function checkable($type, $name, $value, $checked, $options)
+    protected function checkable(string $type, string $name, string $value, bool $checked = false, array $options = []): string
     {
         $checked = $this->getCheckedState($type, $name, $value, $checked);
 

commit df8bbd7c67d9ebf319128631d7640a3341f62aa0

Fix for placeholder default block (#96)
Restores previous default value for BlockBuilder placeholder/get methods (changed in https://github.com/wintercms/storm/commit/1a46e9e9ecea3e71c15cbe8174708724466bcc15 which caused issues as the `placeholderFunction` method in the CMS module was checking for null, not an empty string. 

Technically speaking a block that doesn't exist with no default value provided should return null, so the original default parameter values were correct.

src/Html/BlockBuilder.php
@@ -103,7 +103,7 @@ class BlockBuilder
      *
      * If the block does not exist, then the `$default` content will be returned instead.
      */
-    public function placeholder(string $name, string $default = ''): string
+    public function placeholder(string $name, string $default = null): ?string
     {
         $result = $this->get($name, $default);
         unset($this->blocks[$name]);
@@ -120,7 +120,7 @@ class BlockBuilder
      *
      * If the block does not exist, then the `$default` content will be returned instead.
      */
-    public function get(string $name, string $default = ''): string
+    public function get(string $name, string $default = null): ?string
     {
         if (!isset($this->blocks[$name])) {
             return $default;

tests/Html/BlockBuilderTest.php
@@ -229,4 +229,13 @@ class BlockBuilderTest extends TestCase
         );
         $this->assertEquals('In between', $content);
     }
+
+    public function testGetBlock()
+    {
+        $result = $this->Block->get('non-existent-block');
+        $this->assertNull($result);
+
+        $result = $this->Block->get('non-existent-block', 'default value');
+        $this->assertEquals('default value', $result);
+    }
 }