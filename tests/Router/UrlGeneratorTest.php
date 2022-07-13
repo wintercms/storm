wintercms/storm

commit 32df10c002b7b522bdac054fc991d74b73533f98

Avoid issues coming from double encoded URLs
Fixes https://github.com/wintercms/wn-translate-plugin/issues/36, replaces https://github.com/wintercms/storm/pull/88. Credit to @mjauvin for the initial test case.

src/Router/UrlGenerator.php
@@ -65,10 +65,10 @@ class UrlGenerator extends UrlGeneratorBase
 
         // Parse input
         if (is_string($url)) {
-            $url = parse_url($url);
+            $url = parse_url(urldecode($url));
         }
         if (is_string($replace)) {
-            $replace = parse_url($replace);
+            $replace = parse_url(urldecode($replace));
         }
 
         // Prepare input data
@@ -285,9 +285,6 @@ class UrlGenerator extends UrlGeneratorBase
                 $queryParams = $url['query'];
             }
 
-            if (is_array($url['query'])) {
-                $url['query'] = static::buildStr($url['query']);
-            }
             $urlString .= '?' . static::buildStr($queryParams);
         }
 

tests/Router/UrlGeneratorTest.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use \Winter\Storm\Router\UrlGenerator;
+use Winter\Storm\Router\UrlGenerator;
 use Illuminate\Routing\RouteCollection;
 
 class UrlGeneratorTest extends TestCase
@@ -509,5 +509,28 @@ class UrlGeneratorTest extends TestCase
             'https://user:pass@github.com/wintercms/winter',
             http_build_url($segments, [], HTTP_URL_STRIP_PORT | HTTP_URL_STRIP_QUERY | HTTP_URL_STRIP_FRAGMENT)
         );
+    }
+
+    public function testQueryArgsArrayMatchLaravel()
+    {
+        $checkUrlsOnBoth = [
+            // Non url-encoded
+            'https://www.example.com/path?array[]=v1&array[]=v2',
+
+            // Already url-encoded
+            'https://www.example.com/path?array%5B%5D=v1&array%5B%5D=v2',
+        ];
+
+        foreach ($checkUrlsOnBoth as $url) {
+            $generator = new \Winter\Storm\Router\UrlGenerator(new RouteCollection, Request::create($url));
+            $baseGenerator = new \Illuminate\Routing\UrlGenerator(new RouteCollection, Request::create($url));
+
+            // @NOTE: Laravel does not guarantee that the URL generator always returns
+            // correctly URL encoded URLs - however Winter's does. Decode before comparing.
+            $this->assertEquals(
+                urldecode($baseGenerator->to($url)),
+                urldecode($generator->to($url))
+            );
+        }
     }
 }

commit 768c2c4fbc5c2cb87d0321e2f190a857b976777d

Only decode query arguments before parsing the URL (#95)
Fixes issue introduced in 32df10c that caused double encoded URLs present in a URL's path (double encoded to survive Laravel's router that decodes path segments before routing) to be decoded during UrlGenerator actions which would break them as the output would no longer be double encoded as was specifically provided to the input. 

The above commit broke the ImageResizer encoded URLs in wintercms/winter

src/Router/UrlGenerator.php
@@ -63,12 +63,21 @@ class UrlGenerator extends UrlGeneratorBase
                    | HTTP_URL_STRIP_PASS;
         }
 
+        // Decode query parameters before parsing the URL
+        $decodeQueryParams = function (string $url): string {
+            if (Str::contains($url, '?')) {
+                list($urlWithoutQuery, $queryArgs) = explode('?', $url, 2);
+                $url = $urlWithoutQuery . '?' . urldecode($queryArgs);
+            }
+            return $url;
+        };
+
         // Parse input
         if (is_string($url)) {
-            $url = parse_url(urldecode($url));
+            $url = parse_url($decodeQueryParams($url));
         }
         if (is_string($replace)) {
-            $replace = parse_url(urldecode($replace));
+            $replace = parse_url($decodeQueryParams($replace));
         }
 
         // Prepare input data

tests/Router/UrlGeneratorTest.php
@@ -537,4 +537,32 @@ class UrlGeneratorTest extends TestCase
             );
         }
     }
+
+    public function testEncodedUrlInPathMatchLaravel()
+    {
+        $urlInPath = 'https://testUrlInPath/path?k1=v1&k2';
+        $url = 'https://testdomain/' . rawurlencode($urlInPath);
+
+        $generator = new \Winter\Storm\Router\UrlGenerator(new RouteCollection, Request::create($url));
+        $baseGenerator = new \Illuminate\Routing\UrlGenerator(new RouteCollection, Request::create($url));
+
+        $this->assertEquals(
+            urldecode($baseGenerator->to($url)),
+            urldecode($generator->to($url))
+        );
+    }
+
+    public function testDoublyEncodedUrlInPathMatchLaravel()
+    {
+        $urlInPath = 'https://testUrlInPath/path?k1=v1&k2';
+        $url = 'https://testdomain/' . rawurlencode(rawurlencode($urlInPath));
+
+        $generator = new \Winter\Storm\Router\UrlGenerator(new RouteCollection, Request::create($url));
+        $baseGenerator = new \Illuminate\Routing\UrlGenerator(new RouteCollection, Request::create($url));
+
+        $this->assertEquals(
+            urldecode($baseGenerator->to($url)),
+            urldecode($generator->to($url))
+        );
+    }
 }