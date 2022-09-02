a17fc26b53443a92fa0ac3a2277975a1af98b116
Ben Thomson <git@alfreido.com>
2022-09-02 16:41:55 +0800

    Ensure cached routes are loaded on app boot
    Simplifies the routing service provider and checks if routes are cached. If so, this will load the cached routes on the app being booted.
    
    Fixes https://github.com/wintercms/winter/issues/666

--- src/Router/RoutingServiceProvider.php
+++ src/Router/RoutingServiceProvider.php
@@ -5,6 +5,22 @@ use Illuminate\Routing\RoutingServiceProvider as RoutingServiceProviderBase;
 class RoutingServiceProvider extends RoutingServiceProviderBase
 {
     /**
+     * Boot the service provider.
+     *
+     * If routes are cached, ensure that the cached routes are loaded when the app is booted.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->routesAreCached()) {
+            $this->app->booted(function () {
+                require $this->app->getCachedRoutesPath();
+            });
+        }
+    }
+
+    /**
      * Register the router instance.
      *
      * @return void
@@ -13,49 +29,6 @@ class RoutingServiceProvider extends RoutingServiceProviderBase
     {
         $this->app->singleton('router', function ($app) {
             return new CoreRouter($app['events'], $app);
-        });
-    }
-
-    /**
-     * Register the URL generator service.
-     *
-     * @return void
-     */
-    protected function registerUrlGenerator()
-    {
-        $this->app->singleton('url', function ($app) {
-            $routes = $app['router']->getRoutes();
-
-            // The URL generator needs the route collection that exists on the router.
-            // Keep in mind this is an object, so we're passing by references here
-            // and all the registered routes will be available to the generator.
-            $app->instance('routes', $routes);
-
-            $url = new UrlGenerator(
-                $routes,
-                $app->rebinding(
-                    'request',
-                    $this->requestRebinder()
-                ),
-                $app['config']['app.asset_url'] ?? null
-            );
-
-            $url->setSessionResolver(function () {
-                return $this->app['session'];
-            });
-
-            $url->setKeyResolver(function () {
-                return $this->app->make('config')->get('app.key');
-            });
-
-            // If the route collection is "rebound", for example, when the routes stay
-            // cached for the application, we will need to rebind the routes on the
-            // URL generator instance so it has the latest version of the routes.
-            $app->rebinding('routes', function ($app, $routes) {
-                $app['url']->setRoutes($routes);
-            });
-
-            return $url;
         });
     }
 }
