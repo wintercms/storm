e4a28c3816268e3d426be93ca8f5a3770415bb99
Luke Towers <luke@luketowers.ca>
2022-09-20 21:05:48 -0600

    Add test cases for Arr::moveKeyToIndex()

--- tests/Support/ArrTest.php
+++ tests/Support/ArrTest.php
@@ -4,6 +4,52 @@ use Winter\Storm\Support\Arr;
 
 class ArrTest extends TestCase
 {
+    public function testMoveKeyToIndex()
+    {
+        $array = [
+            'one' => 'a',
+            'two' => 'b',
+            'three' => 'c',
+        ];
+
+        // 0 based index means that index 1 is the second element
+        $this->assertSame([
+            'one' => 'a',
+            'three' => 'c',
+            'two' => 'b',
+        ], Arr::moveKeyToIndex($array, 'three', 1));
+
+        // 0 index inserts at start
+        $this->assertSame([
+            'two' => 'b',
+            'one' => 'a',
+            'three' => 'c',
+        ], Arr::moveKeyToIndex($array, 'two', 0));
+
+        // Index out of range inserts at end
+        $this->assertSame([
+            'one' => 'a',
+            'three' => 'c',
+            'two' => 'b',
+        ], Arr::moveKeyToIndex($array, 'two', 10));
+
+        // Negative index inserting as first element
+        $this->assertSame([
+            'two' => 'b',
+            'one' => 'a',
+            'three' => 'c',
+        ], Arr::moveKeyToIndex($array, 'two', -10));
+
+        // Elements with null values are correctly able to be sorted
+        $nullValueArray = $array;
+        $nullValueArray['two'] = null;
+        $this->assertSame([
+            'one' => 'a',
+            'three' => 'c',
+            'two' => null,
+        ], Arr::moveKeyToIndex($nullValueArray, 'two', 2));
+    }
+
     public function testArrClass()
     {
         $array = [
