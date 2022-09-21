5c46aa3ebe96370489ee6e02589582e0ba804dd0
Jack Wilkinson <31214002+jaxwilko@users.noreply.github.com>
2022-09-21 17:37:38 +0100

    Added Str::unique() & Arr::moveKeyToIndex() helpers (#114)

--- src/Support/Arr.php
+++ src/Support/Arr.php
@@ -1,22 +1,19 @@
 <?php namespace Winter\Storm\Support;
 
 use Illuminate\Support\Arr as ArrHelper;
+use InvalidArgumentException;
 
 /**
  * Array helper
  *
- * @author Alexey Bobkov, Samuel Georges
+ * @author Winter CMS
  */
 class Arr extends ArrHelper
 {
     /**
      * Build a new array using a callback.
-     *
-     * @param  array  $array
-     * @param  callable  $callback
-     * @return array
      */
-    public static function build(array $array, callable $callback)
+    public static function build(array $array, callable $callback): array
     {
         $results = [];
 
@@ -27,5 +24,27 @@ class Arr extends ArrHelper
         }
 
         return $results;
+    }
+
+    /**
+     * Moves the key to the index within the array, negative index will work backwards from the end of the array
+     * @throws InvalidArgumentException if the key does not exist in the array
+     */
+    public static function moveKeyToIndex(array $array, string|int $targetKey, int $index): array
+    {
+        if (!array_key_exists($targetKey, $array)) {
+            throw new InvalidArgumentException(sprintf('Key "%s" does not exist in the array', $targetKey));
+        }
+
+        $keys = array_diff(array_keys($array), [$targetKey]);
+
+        array_splice($keys, $index, 0, [$targetKey]);
+
+        $sorted = [];
+        foreach ($keys as $key) {
+            $sorted[$key] = $array[$key];
+        }
+
+        return $sorted;
     }
 }
--- tests/Support/StrTest.php
+++ tests/Support/StrTest.php
@@ -15,4 +15,21 @@ class StrTest extends TestCase
         $this->assertSame('bob or joe', Str::join(['bob', 'joe'], ', ', ', or ', ' or '));
         $this->assertSame('bob; joe; or sally', Str::join(['bob', 'joe', 'sally'], '; ', '; or '));
     }
+
+    public function testUnique()
+    {
+        // Original returned unmodified when already unique
+        $this->assertSame('winter_cms', Str::unique('winter_cms', []));
+        $this->assertSame('winter_cms', Str::unique('winter_cms', ['winter_cms_1', 'winter_cms_2']));
+
+        // // String modified to be the default step higher than the highest index identified
+        $this->assertSame('winter_cms_1', Str::unique('winter_cms', ['winter_cms']));
+        $this->assertSame('winter_cms_4', Str::unique('winter_cms', ['winter_cms', 'winter_cms_1', 'test_5', 'winter_cms_3']));
+
+        // String modified to be the default step higher than the highest index identified with reversed order of items
+        $this->assertSame('winter_cms_98', Str::unique('winter_cms', ['winter_cms', 'winter_cms_97', 'test_5', 'winter_cms_3']));
+
+        // String modified to be the provided step higher than the highest index identified with the provided separator
+        $this->assertSame('winter_cms 5', Str::unique('winter_cms', ['winter_cms', 'winter_cms 1', 'test_5', 'winter_cms 3'], ' ', 2));
+    }
 }
