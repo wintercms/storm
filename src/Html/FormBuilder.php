0c18f8a49831e67bf77be5315aa65238c1f72193
AIC BV <89913092+AIC-BV@users.noreply.github.com>
2023-02-27 09:16:11 +0100

    change !is_null/!empty to !isset
    Co-authored-by: Marc Jauvin <marc.jauvin@gmail.com>

--- src/Html/FormBuilder.php
+++ src/Html/FormBuilder.php
@@ -286,7 +286,7 @@ class FormBuilder
             $merge = compact('type', 'value', 'id');
 
             $options = array_filter(array_merge($options, $merge), function ($item) {
-                return !empty($item);
+                return !isset($item);
             });
         }
 
