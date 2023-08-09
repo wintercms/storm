35ae43485cd0c50bd7f1b59ba10fc893bfe9c1ad
Marc Jauvin <marc.jauvin@gmail.com>
2023-08-09 12:29:01 -0400

    add explanation comment

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -230,6 +230,7 @@ class Model extends EloquentModel implements ModelInterface
                         // register a default priority(0) listener to call the model method
                         $model->bindEvent('model.' . $method, [$model, $method]);
                     }
+                    // Use a halting event; First handler that returns a value wins.
                     return $model->fireEvent('model.' . $method, halt:true);
                 });
             }
