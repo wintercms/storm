3e27376e63fc6f1d0e72021950a4f55f84522a30
Marc Jauvin <marc.jauvin@gmail.com>
2024-05-03 11:08:48 -0400

    only use RuntimeException

--- src/Console/Process.php
+++ src/Console/Process.php
@@ -1,7 +1,6 @@
 <?php namespace Winter\Storm\Console;
 
 use Symfony\Component\Process\Process as BaseProcess;
-use Winter\Storm\Exception\ApplicationException;
 
 /**
  * Process class
@@ -16,13 +15,12 @@ class Process extends BaseProcess
      *
      * @return $this
      *
-     * @throws RuntimeException In case the TTY mode is not supported
-     * @throws ApplicationException In case /dev/tty is not accessible
+     * @throws RuntimeException In case the TTY mode is not supported or /dev/tty is not accessible.
      */
     public function setTty(bool $tty): static
     {
         if (($openBasedir = ini_get('open_basedir')) && !@is_readable('/dev/tty')) {
-            throw new ApplicationException("\nYou have PHP open_basedir restricted for your environment.\nTTY mode has been disabled.\n");
+            throw new RuntimeException("\nYou have PHP open_basedir restricted for your environment.\nTTY mode has been disabled.\n");
         }
 
         return parent::setTty($tty);
