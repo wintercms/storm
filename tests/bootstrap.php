3ffb684f2bc9c34365fb39e1ea9595d158b60345
Marc Jauvin <marc.jauvin@gmail.com>
2024-10-21 12:30:32 -0400

    add GrammarTestCase

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,4 @@
 
 class_alias('Winter\Storm\Tests\TestCase', 'TestCase');
 class_alias('Winter\Storm\Tests\DbTestCase', 'DbTestCase');
+class_alias('Winter\Storm\Tests\GrammarTestCase', 'GrammarTestCase');
