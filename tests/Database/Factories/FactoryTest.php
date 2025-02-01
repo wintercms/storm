e8711386e4f8a3af9273bf8e4d76a9147b3472f2
Luke Towers <github@luketowers.ca>
2025-01-31 23:53:35 -0600

    Add test case for modules

--- tests/Database/Factories/FactoryTest.php
+++ tests/Database/Factories/FactoryTest.php
@@ -6,6 +6,9 @@ class FactoryTest extends TestCase
 {
     public function testResolveFactoryName()
     {
+        $factoryClass = Factory::resolveFactoryName('Module\Models\TestModel');
+        $this->assertEquals($factoryClass, 'Module\Database\Factories\TestModelFactory');
+
         $factoryClass = Factory::resolveFactoryName('Plugin\Author\Models\TestModel');
         $this->assertEquals($factoryClass, 'Plugin\Author\Database\Factories\TestModelFactory');
 
