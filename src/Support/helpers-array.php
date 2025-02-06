489f7f8c04a69d3b958fcbe05a43d3d73704434f
João Costa <joaocosta.azores@gmail.com>
2025-02-05 23:32:33 -0100

    Update src/Support/helpers-array.php
    Co-authored-by: Ben Thomson <git@alfreido.com>

--- src/Support/helpers-array.php
+++ src/Support/helpers-array.php
@@ -141,7 +141,7 @@ if (!function_exists('array_last')) {
      * @param  mixed  $default
      * @return mixed
      */
-    function array_last($array, callable|null $callback = null, $default = null)
+    function array_last($array, ?callable $callback = null, $default = null)
     {
         return Arr::last($array, $callback, $default);
     }
