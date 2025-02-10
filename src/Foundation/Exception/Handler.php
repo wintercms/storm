4734d957a31102cd615e6adaf776329ff8f7e496
Jack Wilkinson <31214002+jaxwilko@users.noreply.github.com>
2025-02-10 15:48:12 +0000

    Removed debug code

--- src/Foundation/Exception/Handler.php
+++ src/Foundation/Exception/Handler.php
@@ -69,9 +69,6 @@ class Handler extends ExceptionHandler
             return;
         }
 
-        ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-
         if (class_exists('Log')) {
             Log::error($throwable->getMessage(), $this->parseExecption($throwable));
         }
