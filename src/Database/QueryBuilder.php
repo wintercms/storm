94650d118238c78cd71d70782eb10fa9241be051
Marc Jauvin <marc.jauvin@gmail.com>
2025-07-22 09:58:11 -0400

    embrace laravel when we can

--- src/Database/QueryBuilder.php
+++ src/Database/QueryBuilder.php
@@ -313,35 +313,7 @@ class QueryBuilder extends QueryBuilderBase
 
         $this->clearDuplicateCache();
 
-        if ($update === []) {
-            return (int) $this->insert($values);
-        }
-
-        if (!is_array(reset($values))) {
-            $values = [$values];
-        } else {
-            foreach ($values as $key => $value) {
-                ksort($value);
-
-                $values[$key] = $value;
-            }
-        }
-
-        if (is_null($update)) {
-            $update = array_keys(reset($values));
-        }
-
-        $bindings = $this->cleanBindings(array_merge(
-            Arr::flatten($values, 1),
-            collect($update)->reject(function ($value, $key) {
-                return is_int($key);
-            })->all()
-        ));
-
-        return $this->connection->affectingStatement(
-            $this->grammar->compileUpsert($this, $values, (array) $uniqueBy, $update),
-            $bindings
-        );
+        return parent::upsert($values, $uniqueBy, $update);
     }
 
     /**
