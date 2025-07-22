83159e4a9615b81606fe71ca46789d173979c0ea
Marc Jauvin <marc.jauvin@gmail.com>
2025-07-22 10:08:47 -0400

    fix typo in class aliases

--- src/Database/Query/Grammars/MariaDbGrammar.php
+++ src/Database/Query/Grammars/MariaDbGrammar.php
@@ -1,9 +1,9 @@
 <?php namespace Winter\Storm\Database\Query\Grammars;
 
-use Illuminate\Database\Query\Grammars\MariaDbGrammar as BaseMariaDbGrammer;
+use Illuminate\Database\Query\Grammars\MariaDbGrammar as BaseMariaDbGrammar;
 use Winter\Storm\Database\Query\Grammars\Concerns\SelectConcatenations;
 
-class MariaDbGrammar extends BaseMariaDbGrammer
+class MariaDbGrammar extends BaseMariaDbGrammar
 {
     use SelectConcatenations;
 }
