1291e7ff3386a08ebb2ffe0e9c67da9d074d4c72
Jack Wilkinson <me@jackwilky.com>
2025-09-03 15:29:06 +0100

    Added test case and fixed issue with overwriting app from parent

--- tests/Foundation/ApplicationTest.php
+++ tests/Foundation/ApplicationTest.php
@@ -5,31 +5,85 @@ use Winter\Storm\Filesystem\PathResolver;
 
 class ApplicationTest extends TestCase
 {
+    protected Application $fakeApp;
     protected string $basePath;
 
     protected function setUp(): void
     {
         // Mock application
         $this->basePath = '/tmp/custom-path';
-        $this->app = new Application($this->basePath);
+        $this->fakeApp = new Application($this->basePath);
     }
 
     public function testPathMethods()
     {
-        $this->assertEquals(PathResolver::join($this->basePath, '/plugins'), $this->app->pluginsPath());
-        $this->assertEquals(PathResolver::join($this->basePath, '/themes'), $this->app->themesPath());
-        $this->assertEquals(PathResolver::join($this->basePath, '/storage/temp'), $this->app->tempPath());
-        $this->assertEquals(PathResolver::join($this->basePath, '/storage/app/uploads'), $this->app->uploadsPath());
-        $this->assertEquals(PathResolver::join($this->basePath, '/storage/app/media'), $this->app->mediaPath());
+        $this->assertEquals(
+            PathResolver::join($this->basePath, '/plugins'),
+            $this->fakeApp->pluginsPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($this->basePath, '/themes'),
+            $this->fakeApp->themesPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($this->basePath, '/storage/temp'),
+            $this->fakeApp->tempPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($this->basePath, '/storage/app/uploads'),
+            $this->fakeApp->uploadsPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($this->basePath, '/storage/app/media'),
+            $this->fakeApp->mediaPath()
+        );
 
         $storagePath = $this->basePath . '/storage';
 
-        $this->assertEquals(PathResolver::join($storagePath, '/framework/config.php'), $this->app->getCachedConfigPath());
-        $this->assertEquals(PathResolver::join($storagePath, '/framework/routes.php'), $this->app->getCachedRoutesPath());
-        $this->assertEquals(PathResolver::join($storagePath, '/framework/compiled.php'), $this->app->getCachedCompilePath());
-        $this->assertEquals(PathResolver::join($storagePath, '/framework/services.php'), $this->app->getCachedServicesPath());
-        $this->assertEquals(PathResolver::join($storagePath, '/framework/packages.php'), $this->app->getCachedPackagesPath());
-        $this->assertEquals(PathResolver::join($storagePath, '/framework/classes.php'), $this->app->getCachedClassesPath());
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/production.config.php'),
+            $this->fakeApp->getCachedConfigPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/routes.php'),
+            $this->fakeApp->getCachedRoutesPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/compiled.php'),
+            $this->fakeApp->getCachedCompilePath()
+        );
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/services.php'),
+            $this->fakeApp->getCachedServicesPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/packages.php'),
+            $this->fakeApp->getCachedPackagesPath()
+        );
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/classes.php'),
+            $this->fakeApp->getCachedClassesPath()
+        );
+    }
+
+    public function testCachedConfigPath()
+    {
+        $storagePath = $this->basePath . '/storage';
+
+        // No env set
+        $this->assertEquals(
+            PathResolver::join($storagePath, '/framework/production.config.php'),
+            $this->fakeApp->getCachedConfigPath()
+        );
+
+        // Test that setting the app env to each value results in the correct config file being returned
+        foreach (['test', 'prod', 'local', 'dev'] as $env) {
+            $this->fakeApp->detectEnvironment(fn() => $env);
+            $this->assertEquals(
+                PathResolver::join($storagePath, '/framework/' . $env . '.config.php'),
+                $this->fakeApp->getCachedConfigPath()
+            );
+        }
     }
 
     public function testSetPathMethods()
@@ -40,9 +94,9 @@ class ApplicationTest extends TestCase
 
             $path = PathResolver::join('/my'.ucfirst($type), '/custom/path');
             $expected = PathResolver::standardize($path);
-            $this->app->{$setter}($path);
+            $this->fakeApp->{$setter}($path);
 
-            $this->assertEquals($expected, $this->app->{$getter}());
+            $this->assertEquals($expected, $this->fakeApp->{$getter}());
         }
     }
 }
